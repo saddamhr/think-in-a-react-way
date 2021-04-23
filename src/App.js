6680b4660d66c77c0b897a2b3dd7f76934fd5973
Md. Saddam Hossain <saddambubt65@gmail.com>
2021-04-24 01:03:52 +0600

    react-form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import Clock from './components/Clock';
+import Form from './components/Form';
 
-const App = () => (
-    <div>
-        <Clock local="bn-BD" />
-    </div>
-);
+function App() {
+    return (
+        <div>
+            <Form />
+        </div>
+    );
+}
 
 export default App;
